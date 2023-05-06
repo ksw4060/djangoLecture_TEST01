0196415467f3ba3c830083a582318223e326e542
Kim SungWoo <gokorea1214@kakao.com>
2023-05-06 23:19:12 +0900

    settings.py 에 auth_user_model 추가 안해서, 추가했음

--- djangoinit/settings.py
+++ djangoinit/settings.py
@@ -5,7 +5,7 @@ from os import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+AUTH_USER_MODEL = 'user.UserModel'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
