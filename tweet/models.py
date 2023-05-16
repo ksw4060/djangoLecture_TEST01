da49c08f387cbebaf0fe74cfc433996f7e219b46
Kim SungWoo <gokorea1214@kakao.com>
2023-05-16 16:32:15 +0900

    change edit

--- tweet/models.py
+++ tweet/models.py
@@ -6,7 +6,7 @@ from user.models import UserModel
 # Create your models here.
 class TweetModel(models.Model):
     class Meta:
-        db_table = "tweet"
+        db_table = "tweet" #db에 있는 table 이름이 tweet이다.
 
     author = models.ForeignKey(UserModel, on_delete=models.CASCADE)
     content = models.CharField(max_length=256)
--- user/models.py
+++ user/models.py
@@ -1,12 +1,8 @@
-# user/models.py
 from django.db import models
 
-
-# Create your models here.
 class UserModel(models.Model):
     class Meta:
         db_table = "my_user"
-    
 
     username = models.CharField(max_length=20, null=False)
     password = models.CharField(max_length=256, null=False)
