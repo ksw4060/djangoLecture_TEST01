e89d0a9d8c4fac359acdda823fa188cd88a7eb9f
Kim SungWoo <gokorea1214@kakao.com>
2023-05-19 23:37:20 +0900

    wool test pinish

--- tweet/urls.py
+++ tweet/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('tweet/', views.tweet, name='tweet'),
-    path('tweet/delete/<int:id>', views.delete_tweet, name='delete-tweet'),
+    path('tweet/delete/<int:id>', views.delete_tweet, name='delete-tweet'), # 게시글 삭제
+    path('tweet/<int:id>', views.detail_tweet, name='detail-tweet'), # 게시글 디테일 뷰
+    path('tweet/comment/<int:id>', views.write_comment, name='write-comment'), # 댓글 쓰기
+    path('tweet/comment/delete/<int:id>', views.delete_comment, name='delete-comment'), #댓글 삭제
 ]
